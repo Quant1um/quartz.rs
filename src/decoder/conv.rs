e9547563ef65d20f0e51cda8a7c043a2ca9d396a
Quant1um <spitfirexv22@gmail.com>
2022-02-16 12:25:26 +0300

    decoding stufff

---
+++ src/decoder/conv.rs
@@ -0,0 +1,104 @@
+use crate::audio::AudioFormat;
+use symphonia::core::audio::{AudioBufferRef, AudioBuffer, Signal};
+use symphonia::core::conv::IntoSample;
+use symphonia::core::sample::Sample;
+
+pub struct Buffer {
+    buffer: Vec<f32>,
+    ptr: usize
+}
+
+impl Buffer {
+
+    pub fn take(mut self, mut dest: &mut [f32]) -> Result<Buffer, usize> {
+        let mut src = &self.buffer[self.ptr..];
+
+        if src.len() > dest.len() {
+            src = &src[..dest.len()];
+            dest.copy_from_slice(src);
+            self.ptr += src.len();
+            Ok(self)
+        } else {
+            dest = &mut dest[..src.len()];
+            dest.copy_from_slice(src);
+            Err(dest.len())
+        }
+    }
+}
+
+pub struct Converter {
+    converter: samplerate::Samplerate,
+    channels_in: u8,
+    channels_out: u8
+}
+
+impl Converter {
+
+    pub fn new(converter: samplerate::ConverterType, src: AudioFormat, dest: AudioFormat) -> Result<Self, samplerate::Error> {
+        let converter = samplerate::Samplerate::new(
+            converter,
+            src.sample_rate,
+            dest.sample_rate,
+            dest.channels as usize)?;
+
+        Ok(Self {
+            converter,
+            channels_in: src.channels,
+            channels_out: dest.channels
+        })
+    }
+
+    pub fn convert(&mut self, source: AudioBufferRef, last: bool) -> Result<Buffer, samplerate::Error> {
+        match source {
+            AudioBufferRef::U8(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::U16(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::U24(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::U32(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::S8(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::S16(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::S24(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::S32(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::F32(buf) => self.convert_typed(&buf, last),
+            AudioBufferRef::F64(buf) => self.convert_typed(&buf, last),
+        }
+    }
+
+    pub fn convert_typed<F: Sample + IntoSample<f32>>(&mut self, source: &AudioBuffer<F>, last: bool) -> Result<Buffer, samplerate::Error> {
+        let mut buffer = vec![0.0; source.frames() * self.channels_in as usize];
+
+        //change channel layout
+
+        match self.channels_out {
+            1 => {
+                let chan = source.chan(0);
+
+                for (src, dest) in chan.iter().zip(buffer.iter_mut()) {
+                    *dest = (*src).into_sample();
+                }
+            },
+
+            2 => {
+                let left = source.chan(0);
+                let right = if source.spec().channels.count() < 2 { source.chan(0) } else { source.chan(1) };
+
+                for ((left, right), dest) in left.iter().zip(right.iter()).zip(buffer.chunks_mut(2)) {
+                    dest[0] = (*left).into_sample();
+                    dest[1] = (*right).into_sample();
+                }
+            },
+
+            xch => panic!("unsupported number of out channels: {}", xch)
+        }
+
+        let buffer = if last {
+            self.converter.process_last(&buffer)
+        } else {
+            self.converter.process(&buffer)
+        }?;
+
+        Ok(Buffer {
+            buffer,
+            ptr: 0
+        })
+    }
+}
--- src/decoder/mod.rs
+++ src/decoder/mod.rs
@@ -1,17 +1,96 @@
-use crate::broadcast::AudioSource;
-use symphonia::core::formats::FormatReader;
-use symphonia::core::codecs::Decoder;
+mod conv;
+
+use crate::audio::{AudioFormat, AudioSource};
+use conv::{Buffer, Converter};
+use symphonia::core::formats::{FormatReader, FormatOptions};
+use symphonia::core::codecs::{Decoder, DecoderOptions};
+use symphonia::core::io::MediaSourceStream;
+use symphonia::core::errors::Error as SymphoniaError;
+use symphonia::default::*;
+use thiserror::Error;
+use symphonia::core::probe::Hint;
+use symphonia::core::meta::MetadataOptions;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("error while reading data: {0}")]
+    Reader(#[from] SymphoniaError),
+    #[error("error while resampling: {0}")]
+    Converter(#[from] samplerate::Error)
+}
 
 pub struct AudioDecoder {
     reader: Box<dyn FormatReader>,
-    decoder: Box<dyn Decoder>
+    decoder: Box<dyn Decoder>,
+    track: u32,
+
+    format: AudioFormat,
+    converter: Converter,
+    buffer: Option<Buffer>
 }
 
-/*
-impl AudioSource for AudioDecoder {
-    type Error = ();
+impl AudioDecoder {
 
-    fn pull(&mut self, samples: &mut [f32]) -> Result<(), Self::Error> {
-        self.source.next_packet()
+    pub fn new(stream: MediaSourceStream, converter: samplerate::ConverterType, format: AudioFormat) -> Result<Self, Error> {
+        let probe = get_probe().
+            format(&Hint::new(),
+                   stream,
+                   &FormatOptions::default(),
+                   &MetadataOptions::default())?;
+
+        let reader = probe.format;
+        let track = reader.default_track().ok_or(SymphoniaError::DecodeError("no tracks found"))?;
+        let params = &track.codec_params;
+        let src_format = AudioFormat {
+            channels: params.channels.ok_or(SymphoniaError::DecodeError("no channel metadata"))?.count() as u8,
+            sample_rate: params.sample_rate.ok_or(SymphoniaError::DecodeError("no sample rate metadata"))?
+        };
+
+        let decoder = get_codecs().make(&track.codec_params, &DecoderOptions { verify: true })?;
+        let converter = conv::Converter::new(converter, src_format, format)?;
+
+        Ok(Self {
+            track: track.id,
+            buffer: None,
+
+            reader,
+            decoder,
+            converter,
+            format
+        })
     }
-}*/
+}
+
+impl AudioSource for AudioDecoder {
+    type Error = Error;
+
+    fn format(&self) -> AudioFormat {
+        self.format
+    }
+
+    fn pull(&mut self, mut samples: &mut [f32]) -> Result<(), Self::Error> {
+        loop {
+            if let Some(buffer) = self.buffer.take() {
+                match buffer.take(samples) {
+                    Ok(buffer) => {
+                        self.buffer = Some(buffer);
+                        return Ok(())
+                    },
+
+                    Err(written) => {
+                        samples = &mut samples[written..];
+                    }
+                }
+            }
+
+            let packet = self.reader.next_packet()?;
+
+            if packet.track_id() != self.track {
+                continue;
+            }
+
+            let audio_data = self.decoder.decode(&packet)?;
+            self.buffer = Some(self.converter.convert(audio_data, false)?);
+        }
+    }
+}
